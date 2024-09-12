1a8d3b89e686f28157ba9da401daade779eda543
JAYA SANKAR <128584016+Jayasankardj@users.noreply.github.com>
2024-09-12 10:46:45 +0530

    Update Symbol Table.c

--- Symbol Table.c
+++ Symbol Table.c
@@ -1,78 +1,92 @@
 #include <stdio.h>
 #include <string.h>
-struct symbol
-{
-	char name[10];
-	char val[10];
-	char address[10];
+#include <stdlib.h>
+#define MAX 100
+struct SymbolTableEntry {
+char name[50];
+char type[20];
+int size;
+int address; // Address field (replaces scope)
 };
-struct table
-{
-	symbol s[20];
-	int count;
-};
-table t;
-void insert(char *n,char *v,char *a)
-{
-	strcpy(t.s[t.count].name,n);
-	strcpy(t.s[t.count].val,v);
-	strcpy(t.s[t.count].address,a);
-	t.count++;
-	printf("\nInsertion Succeed\n\n");
+struct SymbolTableEntry symbolTable[MAX];    
+int count = 0;
+
+void insert(char name[], char type[], int size, int address) {
+for (int i = 0; i < count; i++) {
+    if (strcmp(symbolTable[i].name, name) == 0) {
+        printf("Error: Symbol '%s' already exists in the table.\n", name);
+        return;
+    }
 }
-void search(char *n)
-{
-	for(int i=0;i<t.count;i++)
-	{
-		if(strcmp(t.s[i].name,n)==0)
-		{
-			printf("\nSymbol Found!!\n");
-			printf("Symbol Value=%s\n",t.s[i].val);
-			printf("Symbol Address=%s\n\n",t.s[i].address);
-			return;
-		}
-	}
-	printf("Symbol Not Found!!!\n\n");
+strcpy(symbolTable[count].name, name);
+strcpy(symbolTable[count].type, type);
+symbolTable[count].size = size;
+symbolTable[count].address = address;
+count++;
+printf("Symbol '%s' inserted successfully.\n", name);
 }
-void display()
-{
-	printf("\nSYMBOL NAME\t\tVALUE\t\tADDRESS\n");
-	for(int i=0;i<t.count;i++)
-	{
-		printf("%s\t\t\t%s\t\t\t%s\n",t.s[i].name,t.s[i].val,t.s[i].address);
-	}
-	printf("\n");
+void search(char name[]) {
+for (int i = 0; i < count; i++) {
+    if (strcmp(symbolTable[i].name, name) == 0) {
+        printf("Symbol found: Name = %s, Type = %s, Size = %d, Address = %d\n",
+               symbolTable[i].name, symbolTable[i].type, symbolTable[i].size, symbolTable[i].address);
+        return;
+    }
 }
-int main()
-{
-	t.count=0;
-	int c;
-	char n[20],v[20],a[20];
-	while(1)
-	{
-		printf("1.INSERT INTO SYMBOL TABLE\n2.SEARCH SYMBOL\n3.DISPLAT SYMBOL TABLE\nENTER CHOICE=");
-		scanf("%d",&c);
-		switch(c)
-		{
-			case 1:
-				printf("Enter the Name of the Symbol:");
-				scanf("%s",n);
-				printf("Enter the Value of the Symbol:");
-				scanf("%s",v);
-				printf("Enter the Address of the Symbol:");
-				scanf("%s",a);
-				insert(n,v,a);
-				break;
-			case 2:
-				printf("Enter the Symbol to Search:");
-				scanf("%s",n);
-				search(n);
-				break;
-			case 3:
-				display();
-				break;
-			default:
-				printf("Invalid Operation!!\n\n");
-		}
-	}
+printf("Symbol '%s' not found in the symbol table.\n", name);
+}
+void display() {
+if (count == 0) {
+    printf("Symbol table is empty.\n");
+    return;
+}
+printf("\nSymbol Table:\n");
+printf("---------------------------------------------------\n");
+printf("| %-10s | %-10s | %-6s | %-10s |\n", "Name", "Type", "Size", "Address");
+printf("---------------------------------------------------\n");
+for (int i = 0; i < count; i++) {
+    printf("| %-10s | %-10s | %-6d | %-10d |\n", symbolTable[i].name, symbolTable[i].type, symbolTable[i].size, symbolTable[i].address);
+}
+printf("---------------------------------------------------\n");
+}
+int main() {
+int choice;
+char name[50], type[20];
+int size, address;
+while (1) {
+    printf("\nSymbol Table Operations:\n");
+    printf("1. Insert\n");
+    printf("2. Search\n");
+    printf("3. Display\n");
+    printf("4. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            printf("Enter the symbol name: ");
+            scanf("%s", name);
+            printf("Enter the symbol type: ");
+            scanf("%s", type);
+            printf("Enter the symbol size: ");
+            scanf("%d", &size);
+            printf("Enter the symbol address: ");
+            scanf("%d", &address);
+            insert(name, type, size, address);
+            break;
+        case 2:
+            printf("Enter the symbol name to search: ");
+            scanf("%s", name);
+            search(name);
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+
+        default:
+            printf("Invalid choice. Please try again.\n");
+    }
+}
+return 0;
 }
